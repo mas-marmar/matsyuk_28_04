58a98093525ea33f174bc3bcfb5ee0d68984db8f
Студент группы IS <is@sielom.local>
2026-04-28 14:56:02 +0500

    2804

--- someapp/urls.py
+++ someapp/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import AuthorListView, BookListView, BookDetailView
+from . import views
 
 urlpatterns = [
-    path('authors/', AuthorListView.as_view(), name='author_list'),
-    path('authors/<str:author>/books/', BookListView.as_view(), name='book_list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('', views.calculator, name='calculator'),
 ]
--- someapp/views.py
+++ someapp/views.py
@@ -1,34 +1,31 @@
-from django.views.generic import ListView, DetailView
-from django.db.models import Count
-from .models import Book
+from django.shortcuts import render
 
-class AuthorListView(ListView):
-    """Список всех авторов"""
-    template_name = 'author_list.html'
-    context_object_name = 'authors'
-
-    def get_queryset(self):
-        authors = Book.objects.values('author').annotate(
-            book_count=Count('id')
-        ).order_by('author')
-        return authors
-
-class BookListView(ListView):
-    """Список книг конкретного автора"""
-    model = Book
-    template_name = 'book_list.html'
-    context_object_name = 'books'
-
-    def get_queryset(self):
-        author_name = self.kwargs.get('author')
-        return Book.objects.filter(author=author_name)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['author_name'] = self.kwargs.get('author')
-        return context
-
-class BookDetailView(DetailView):
-    model = Book
-    template_name = 'book_detail.html'
-    context_object_name = 'book'
+def calculator(request):
+    result = None
+    error = None
+    
+    if request.method == 'POST':
+        try:
+            num1 = float(request.POST.get('num1', 0))
+            num2 = float(request.POST.get('num2', 0))
+            operation = request.POST.get('operation', '+')
+            
+            if operation == '+':
+                result = num1 + num2
+            elif operation == '-':
+                result = num1 - num2
+            elif operation == '*':
+                result = num1 * num2
+            elif operation == '/':
+                if num2 == 0:
+                    error = 'Низя дилить на нуль!'
+                else:
+                    result = num1 / num2
+                    
+        except (ValueError, TypeError):
+            error = 'ашипка'
+    
+    return render(request, 'calculator.html', {
+        'result': result,
+        'error': error,
+    })
